4f119de042b2df1a3ed268e67ef8a641e604ffaa
vinnylimm <vinny.mvfl12@gmail.com>
2025-05-07 18:02:13 -0300

    ChessException

---
+++ src/chess/ChessException.java
@@ -0,0 +1,10 @@
+package chess;
+
+public class ChessException extends RuntimeException {
+	private static final long SerialVersionUID = 1L;
+
+	public ChessException(String msg){
+		super(msg);
+	}
+
+}
