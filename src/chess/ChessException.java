d3e3eabbdc3adec3fccf1d3bce4a722b16fbfb8d
vinnylimm <vinny.mvfl12@gmail.com>
2025-06-18 12:49:38 -0300

    moving piece

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,6 +1,7 @@
 package chess;
+import boardgame.BoardException;
 
-public class ChessException extends RuntimeException {
+public class ChessException extends BoardException {
 	private static final long SerialVersionUID = 1L;
 
 	public ChessException(String msg){
